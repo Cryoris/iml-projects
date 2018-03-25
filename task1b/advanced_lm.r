f7d20aaa4762a9c3a8fc2b2763e7aad659bfef0f
jgacon <jgacon@student.ethz.ch>
2018-03-25 23:50:11 +0200

    cracked hard BL

--- task1b/advanced_lm.r
+++ task1b/advanced_lm.r
@@ -1,1 +1,19 @@
-?glm
+# Task 1b
+
+setwd("~/uni/semester_8/iml/task1b")
+data <- read.csv("train.csv", header=T)[,-1] # Drop Id column
+x <- as.matrix(data[,-1])
+x_trans <- cbind(x, x^2, exp(x), cos(x))
+y <- as.matrix(data[,1])
+
+
+library(glmnet)
+
+lambdas <- 10^seq(1,-2, by=-0.01)
+model <- cv.glmnet(x_trans, y, alpha=0.8, lambda=lambdas)
+
+coefs <- coef.cv.glmnet(model, s="lambda.1se")
+coefs_ordered <- c(coefs[-1], coefs[1])
+model
+
+write.table(as.matrix(coefs_ordered), "coefs.csv", sep="\n", row.names=F, col.names=F)
