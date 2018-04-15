23fb615dab6aefb2dff92e762a1707402ec5b39a
Jonas Kunath <jonas.kunath@gmx.de>
2018-04-15 17:44:50 +0200

    vif for correlated features

--- task2/MultiClassPred.R
+++ task2/MultiClassPred.R
@@ -9,18 +9,31 @@ library(randomForestSRC)
 library(randomForest)
 library(party)
 
+library(car)
+#needed for "vif", but should not override "makeLearner" from mlr, so lower priority
+
+
 data.train <- read.csv("train.csv", header=T)[,-1] # Drop Id column
 data.test <- read.csv("test.csv", header=T)[,-1] # Drop Id column
 
+##utilize Part 2 (remove parameters aproach)
+
+rem.params = c(-(13+1),-(4+1),-(5+1),-(6+1),-(8+1),-(9+1),-(10+1),-(12+1),-(15+1),-(16+1))
+
+data.train <- data.train[, rem.params]
+data.test <- data.test[, rem.params] 
+
+# from here on, the new approach follows the old one..
 
 ## Create the task
 
 # put data into right format
-train.y = data.train[,1]
+y = data.train[,1]
 x = data.train[,-1]
+#x = data.train[,c(-1,-13)] # feature13=feature12
 
 # y has to be cast from 0/1/2 to 3 TRUE/FALSE-statements
-data.input <- transform(y0 = y==0, y1 = y==1, y2 = y==2, data.train[,-1])
+data.input <- transform(y0 = y==0, y1 = y==1, y2 = y==2, x)
 labels = c("y0", "y1", "y2")
 
 MultiClass.task = makeMultilabelTask(id = "multi", data = data.input, target = labels)
@@ -28,19 +41,18 @@ MultiClass.task = makeMultilabelTask(id = "multi", data = data.input, target = l
 
 ## Constructing a learner: Algorithm adaptation methods
 
-lrn.rfsrc = makeLearner("multilabel.randomForestSRC", bootstrap="by.root")
+lrn.rfsrc = makeLearner("multilabel.randomForestSRC")#, bootstrap="by.root")
 #lrn.rfsrc = makeLearner("regr.randomForest")
 #lrn.rfsrc = makeLearner("multilabel.cforest")
 
 ## Train model
-
-model = train(lrn.rfsrc, MultiClass.task) #, subset = 1:100)
-
+model = mlr::train(lrn.rfsrc, MultiClass.task) #, subset = 1:100)
+  # mlr::train instead of train because car-package overrode train from mlr...
 
 ## Predict
 
-#pred = predict(model, newdata = data.test) 
-pred = predict(model, newdata = x) 
+pred = predict(model, newdata = data.test) 
+#pred = predict(model, newdata = x) 
             # , importance=TRUE,random)
 #performance(pred)
 
@@ -48,10 +60,40 @@ pred = predict(model, newdata = x)
 #   incredibly ugly way to get TRUE/FALSE into 0/1:
 #       as.numeric(as.matrix((as.data.frame(pred)["response.y0"])))
 
-y = 1*as.numeric(as.matrix((as.data.frame(pred)["response.y1"]))) + 2*as.numeric(as.matrix((as.data.frame(pred)["response.y2"])))
+pred.y = 1*as.numeric(as.matrix((as.data.frame(pred)["response.y1"]))) + 2*as.numeric(as.matrix((as.data.frame(pred)["response.y2"])))
 
 
-our.rownames = as.character(1999+c(1:length(y)))
+our.rownames = as.character(1999+c(1:length(pred.y)))
 our.columnnames = c("Id","y")
-write.table(cbind(our.rownames, y), "MultiPredrfsrc_rootBootstrap.csv", sep=",", row.names=F, col.names=our.columnnames)
+write.table(cbind(our.rownames, pred.y), "MultiPredrfsrc_VIFremove.csv", sep=",", row.names=F, col.names=our.columnnames)
+
+###
+#Part 2 : remove correlations
+###
+
+
+######## first approach: cor
+#full.data <- read.csv("train.csv", header=T)[,-1] # Drop Id column
+
+#cor(full.data) 
+# realise 100% correlation between x13&x12; 99% correlation between x12&x6;
+# 99% between x8&x1
+#data <- full.data[,c(-2,-(13+1))]#,-(6+1),-(8+1))] # "+1" for y column
+
+
+####################
+#new approach: vif
+library(car,pos=2)
+
+full.data <- read.csv("train.csv", header=T)[,-1] # Drop Id column
+cor(full.data) 
+# realise 100% correlation between x13&x12 -> aliased variable
+data <- full.data[,c(-2,-(13+1))]  # "+1" for y column
+
+
+vif(glm(x1~x1+x2+x3+x4+x5+x6+x7+x8+x9+x10+x11+x12+x14+x15+x16,data=data))
+#remove GVIF >10: x4,5,6,8,9,10,12,15,16
+rem.columns = c(-(13+1),-(4+1),-(5+1),-(6+1),-(8+1),-(9+1),-(10+1),-(12+1),-(15+1),-(16+1))
+data <- full.data[,rem.columns]
+vif(glm(x14~x1+x2+x3+x7+x11+x14,data=data))
 
